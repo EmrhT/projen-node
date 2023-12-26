776b776564c7588ae494b4cd2653c29840d58ccf
emrht <emrhtfn@gmail.com>
2023-12-26 15:55:41 +0300

    added comment

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,7 @@ const project = new javascript.NodeProject({
 
   minNodeVersion: "14.0.0",
   workflowNodeVersion: "18.1.0", // defaults to minNodeVersion
-  depsUpgrade: true,
+  depsUpgrade: true, // that one line translates into a whole workflow of upgrade-main.yml
   workflow: true
 
   // deps: [],                /* Runtime dependencies of this module. */
