e38b7482ec1b9cc0eca6d2bc94de39a7a5f2e686
emrht <emrhtfn@gmail.com>
2023-12-26 14:41:34 +0300

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,10 @@ const project = new javascript.NodeProject({
   defaultReleaseBranch: "main",
   name: "projen-node",
 
+  minNodeVersion: "18.0.0",
+  workflowNodeVersion: "18.1.0", // defaults to minNodeVersion
+  depsUpgrade: true,
+  workflow: true
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# projen-node
+# Playground Project for testing projen with Node
+1. Initialize with "npx projen new node"
+2. Tweak ".projenrc.js" according to play around.
+3. Run "npx projen" to synth your changes. 
