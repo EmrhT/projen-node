7a35914de016cea719d4fe7a6caf174666eeccca
emrht <emrhtfn@gmail.com>
2023-12-26 23:31:05 +0300

    updated projenrc.js added packageManager

--- .projenrc.js
+++ .projenrc.js
@@ -6,12 +6,21 @@ const project = new javascript.NodeProject({
   minNodeVersion: "14.0.0",
   workflowNodeVersion: "20.1.0", // defaults to minNodeVersion
   depsUpgrade: true, // that one line translates into a whole workflow of upgrade-main.yml
-  workflow: true
+  workflow: true,
+  packageManager: javascript.NodePackageManager.NPM,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+});
+
+const hello_1 = project.addTask('hello_1');
+hello_1.exec('echo hello_1, world!');
+
+const hello_2 = project.addTask('hello_2', {
+  description: 'say hello_2',
+  exec: 'echo hello_2, world!'
 });
 
 project.synth();
